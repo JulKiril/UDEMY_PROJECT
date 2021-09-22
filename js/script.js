39079e32061e182c8546e336a02eef1201bc7329
Iuliia <maxjulval@gmail.com>
2021-09-22 17:33:38 +0300

    used fetch instead of XMLHttpRequest to post form-data

--- js/script.js
+++ js/script.js
@@ -238,27 +238,29 @@ const modal = document.querySelector('.modal'),
                 `;
                 form.insertAdjacentElement('afterend', statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST','server.php');
-                request.setRequestHeader('Content-type', 'application/json');
+
                 const formData = new FormData(form);
                 const obj = {};
                 formData.forEach((value,key) =>{
                     obj[key] = value;
                 });
-                const json = JSON.stringify(obj);
-                request.send(json);
-
-                request.addEventListener('load',() =>{
-                   if(request.status === 200){
-                       console.log(request.response);
-                       showThanksModal(message.sucsess);
-                       form.reset();
-                       statusMessage.remove();
-                    } 
-                   else{
-                       showThanksModal(message.failure);
-                   }
+                
+             
+                fetch('server.php', {
+                    method: 'POST',
+                    body: JSON.stringify(obj),
+                    headers:{ 
+                        'Content-type' : 'application/json'}
+                })
+                .then(data=>data.text())
+                .then((data)=>{
+                    console.log(data);
+                    showThanksModal(message.sucsess);
+                    statusMessage.remove();
+                }).catch(()=>{
+                    showThanksModal(message.failure);
+                }).finally(()=>{
+                    form.reset();
                 });
             });
         }
