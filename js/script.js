f34c8ae015932288f37e89d0ccc6c68d5f89406b
Iuliia <maxjulval@gmail.com>
2021-09-22 12:45:40 +0300

    users inform message modified

--- js/script.js
+++ js/script.js
@@ -101,8 +101,7 @@ setClock('.timer', deadline);
 ///////////////////////MODAL/////////
 
 const modal = document.querySelector('.modal'),
-      modalBtns = document.querySelectorAll('[data-modal]'),
-      modalCloseBtn = document.querySelector('[data-close]');
+      modalBtns = document.querySelectorAll('[data-modal]');
 
       function showModal(){
         modal.classList.add('show');
@@ -115,7 +114,7 @@ const modal = document.querySelector('.modal'),
         item.addEventListener('click',showModal);
       });
 
-      let timeoutId = setTimeout(showModal,10000);
+      let timeoutId = setTimeout(showModal,50000);
       
 
       function hideModal(){
@@ -124,10 +123,8 @@ const modal = document.querySelector('.modal'),
         document.body.style.overflow = '';
       }
 
-      modalCloseBtn.addEventListener('click', hideModal);
-
       modal.addEventListener('click', (e)=>{
-        if(e.target == modal){
+        if(e.target == modal || e.target.getAttribute('data-close') == ""){
             hideModal();
         }
       });
@@ -220,7 +217,7 @@ const modal = document.querySelector('.modal'),
         const forms = document.querySelectorAll('form');
 
         const message = {
-            loading: 'загрузка',
+            loading: 'img/form/spinner.svg',
             sucsess: 'Спасибо, скоро мы с Вами свяжемся',
             failure: 'Что-то пошло не так'
         };
@@ -233,10 +230,13 @@ const modal = document.querySelector('.modal'),
             form.addEventListener('submit',(e)=>{
                 e.preventDefault();
 
-                const statusMessage = document.createElement('div');
-                statusMessage.classList.add('status');
-                statusMessage.textContent = message.loading;
-                form.append(statusMessage);
+                let statusMessage = document.createElement('img');
+                statusMessage.src = message.loading;
+                statusMessage.style.cssText = `
+                    display: block;
+                    margin: 0 auto;
+                `;
+                form.insertAdjacentElement('afterend', statusMessage);
 
                 const request = new XMLHttpRequest();
                 request.open('POST','server.php');
@@ -252,17 +252,37 @@ const modal = document.querySelector('.modal'),
                 request.addEventListener('load',() =>{
                    if(request.status === 200){
                        console.log(request.response);
-                       statusMessage.textContent = message.sucsess;
+                       showThanksModal(message.sucsess);
                        form.reset();
-                       setTimeout(()=>{
-                           statusMessage.remove();
-                       },2000);
-                   } 
+                       statusMessage.remove();
+                    } 
                    else{
-                       statusMessage.textContent = message.failure;
+                       showThanksModal(message.failure);
                    }
                 });
             });
         }
 
+        function showThanksModal(message){
+            const prevModalDilog = document.querySelector('.modal__dialog');
+            prevModalDilog.classList.add('hide');
+
+            showModal();
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `
+            <div class="modal__content"> 
+                <div data-close class="modal__close">&times;</div>        
+                <div class="modal__title">${message}</div>               
+            </div>
+            `;
+            document.querySelector('.modal').append(thanksModal);
+
+            setTimeout(()=>{
+                thanksModal.remove();
+                prevModalDilog.classList.add('show');
+                prevModalDilog.classList.remove('hide');
+                hideModal();
+            },4000);
+        }
     });
