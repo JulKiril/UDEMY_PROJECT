56cff1a5de177028651600a48aeeda943e7baa4e
Iuliia <maxjulval@gmail.com>
2021-09-22 11:51:33 +0300

    post forms data added

--- js/script.js
+++ js/script.js
@@ -215,4 +215,54 @@ const modal = document.querySelector('.modal'),
            '11', 
            '.menu .container',
            'menu__item').renderMenuItem();
+
+        /////////POST FORMS DATA//////
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'загрузка',
+            sucsess: 'Спасибо, скоро мы с Вами свяжемся',
+            failure: 'Что-то пошло не так'
+        };
+
+        forms.forEach(form =>{
+            postData(form);
+        });
+
+        function postData(form){
+            form.addEventListener('submit',(e)=>{
+                e.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST','server.php');
+                request.setRequestHeader('Content-type', 'application/json');
+                const formData = new FormData(form);
+                const obj = {};
+                formData.forEach((value,key) =>{
+                    obj[key] = value;
+                });
+                const json = JSON.stringify(obj);
+                request.send(json);
+
+                request.addEventListener('load',() =>{
+                   if(request.status === 200){
+                       console.log(request.response);
+                       statusMessage.textContent = message.sucsess;
+                       form.reset();
+                       setTimeout(()=>{
+                           statusMessage.remove();
+                       },2000);
+                   } 
+                   else{
+                       statusMessage.textContent = message.failure;
+                   }
+                });
+            });
+        }
+
     });
